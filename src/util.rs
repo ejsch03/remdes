b7e8eccea1eb33ab1b56e10e610eb9297d47a6c0
splurf <ejsch03@gmail.com>
2025-12-11 10:04:03 -0500

    refac

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,22 @@
-use std::time::Duration;
+use std::{
+    net::{SocketAddr, ToSocketAddrs},
+    time::Duration,
+};
+
+/// Retrieve a default socket with specified port number.
+pub fn get_socket_addr(port: u16) -> SocketAddr {
+    ("127.0.0.1", port)
+        .to_socket_addrs()
+        .expect("Failed to retrieve socket address(s)")
+        .next()
+        .expect("No available socket address(s)")
+}
 
 /// the duration of a single interval based on the given rate.
 pub const fn tick_dur(rate: f32) -> Duration {
     if !(rate.is_finite() && rate > 0.0) {
         return Duration::ZERO;
     }
-
     let secs_f = 1.0 / rate;
     let secs = secs_f as u64;
     let nanos = ((secs_f - secs as f32) * 1_000_000_000.0) as u32;
